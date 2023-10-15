7f964876c8969a324a8779ac1a383886d29529f0
Menna-Newear <mennanewear@gmail.com>
2023-10-16 02:15:54 +0300

    genrating note model generator file

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,15 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   final String noteTitle;
+  @HiveField(1)
   final String noteContent;
+  @HiveField(2)
   final String noteDate;
+  @HiveField(3)
   final int noteColor;
 
   NoteModel({
