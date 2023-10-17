05c24e8adbf54be88939768bb0704c1bb294dcbf
Menna-Newear <mennanewear@gmail.com>
2023-10-18 00:26:38 +0300

    add Adding notes State

--- lib/addNoteCubit/cubit/add_note_cubit_state.dart
+++ lib/addNoteCubit/cubit/add_note_cubit_state.dart
@@ -4,3 +4,12 @@ part of 'add_note_cubit_cubit.dart';
 sealed class AddNoteCubitState {}
 
 final class AddNoteCubitInitial extends AddNoteCubitState {}
+
+final class AddNoteLoading extends AddNoteCubitState {}
+
+final class AddNoteSuccess extends AddNoteCubitState {}
+
+final class AddNoteFailure extends AddNoteCubitState {
+  final String errMessage;
+  AddNoteFailure(this.errMessage);
+}
