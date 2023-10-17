039e9b8c032284a7e7c0dbf53a29f24a78fe4c82
Menna-Newear <mennanewear@gmail.com>
2023-10-18 01:12:08 +0300

    provide add Note cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/addNoteCubit/cubit/add_note_cubit_cubit.dart';
 import 'package:notes_app/constants.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/homePage.dart';
@@ -16,16 +18,20 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: "Poppins",
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AddNoteCubitCubit(),
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: "Poppins",
+        ),
+        home: HomePage(),
       ),
-      routes: {
-        HomePage.id: (context) => HomePage(),
-      },
-      initialRoute: HomePage.id,
     );
   }
 }
